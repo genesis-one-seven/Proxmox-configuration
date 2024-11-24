f38909c26ef64ce2149ed92e50435c79ae90ac9f
genesis-one-seven <162949163+genesis-one-seven@users.noreply.github.com>
2024-11-24 21:08:11 +0100

    Update register.sh

--- evernode/register.sh
+++ evernode/register.sh
@@ -1,5 +1,7 @@
 wget  https://raw.githubusercontent.com/EvernodeXRPL/evernode-resources/main/sashimono/installer/evernode.sh 
 
+sudo apt install expect
+
 chmod +x evernode.sh
 chmod +x register.exp
 
