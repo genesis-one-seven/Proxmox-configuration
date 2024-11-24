6629749ab4a448dd821ef509c242b6f7c2983d5f
genesis-one-seven <162949163+genesis-one-seven@users.noreply.github.com>
2024-11-24 21:14:19 +0100

    Update register.sh

--- evernode/register.sh
+++ evernode/register.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+echo -n "Please enter the account number: "
+read account
+
+echo -n "Please enter the secrect key: "
+read secret_key
+
+
+
 wget  https://raw.githubusercontent.com/EvernodeXRPL/evernode-resources/main/sashimono/installer/evernode.sh 
 
 sudo apt install expect
@@ -5,6 +15,6 @@ sudo apt install expect
 chmod +x evernode.sh
 chmod +x register.exp
 
-expect ./register.exp [account] [secret] [ordinal] [number] 
+expect ./register.exp $account $secret_key  
 
 
